3ce96a3d0d7a389ffb92a9bd51638bea17ca2dbb
s_nega <snezhana.golovko@gmail.com>
2023-12-13 08:49:28 +0600

    git testing commit

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const bookingRouter = require('./routes/booking');
 const SearchLocation = require('./routes/api/SearchLocations')
-const api_key = '';
+// const api_key = '';
 
 const app = express();
 console.log('hey hey')
 
 const server = http.createServer(app);
-app.set('port' , 4200)
+// app.set('port' , 4200)
 app.use(express.static(__dirname + '/public')); 
-// app.use('/index', indexRouter);
+app.use('/index', indexRouter);
 app.use('/', bookingRouter);
-// app.use('/housing', houseRouter);
+app.use('/housing', houseRouter);
 module.exports = app;
