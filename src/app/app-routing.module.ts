51da5f2509435016b3b899f49a5fd8472152909d
s_nega <snezhana.golovko@gmail.com>
2023-12-20 21:22:04 +0600

    basic auth and reg design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AirTicketsComponent } from './air-tickets/air-tickets.component';
 import { CarRentalComponent } from './car-rental/car-rental.component';
 import { HousingComponent } from './housing/housing.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/housing', pathMatch: 'full' }, // Перенаправление на маршрут "housing" при загрузке приложения
@@ -10,9 +12,9 @@ const routes: Routes = [
   { path: 'car-rental', component: CarRentalComponent },
   { path: 'housing', component: HousingComponent },
 
-  // { path: 'regOblect', component: HousingComponent },
-  // { path: 'reg', component: HousingComponent },
-  // { path: 'auth', component: HousingComponent },
+  { path: 'regOblect', component: HousingComponent },
+  { path: 'reg', component: RegistrationComponent },
+  { path: 'auth', component: AuthorizationComponent },
 ];
 
 @NgModule({
