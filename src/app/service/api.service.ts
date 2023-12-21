1346874715212ab17145af08cd855baef3f878f0
AnKlvy <andreyklimov.ver2@gmail.com>
2023-12-21 20:44:13 +0600

    added HTTP methods in apiservice

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:8080'
-  constructor(private http: HttpClient) { }
+  private apiUrl = 'http://localhost:8080';
+  constructor(private http: HttpClient) {}
   // houseFunc() {
   //   return this.http.get(`${this.apiUrl}/house`);
   // }
   houseFunc() {
     return this.http.get(`${this.apiUrl}/house`);
+  }
+  getHouses(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/api/house`);
+  }
+  getHouse(houseId: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/api/house/${houseId}`);
+  }
+  addHouse(houseData: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/api/house`, houseData);
+  }
+  updateHouse(houseId: string,  updatedData: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/api/house/${houseId}`, updatedData);
+  }
+  deleteHouse(houseId: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/api/house/${houseId}`);
   }
 }
